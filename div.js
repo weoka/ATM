0572de816c1ea8213290daa8c9b17a6eddb5e5a8
Weoka <weokap@gmail.com>
2020-09-26 15:45:28 +0200

    Cashiers's logic

--- div.js
+++ div.js
@@ -4,22 +4,24 @@ class Bill{
         this.type = t;
         this.availableQuantity = a;
     }
-    availability(q){
-        if(this.availableQuantity >= q){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
     totalAvailable(){
         return this.type*this.availableQuantity;
     }
 }
 
-bills = [];
+class Transaction{
+    constructor(q,t){
+        this.quantity = q;
+        this.time = t;
+    }
+}
 
-function addBills(bill, q){
+//defining common variables
+var bills = [];
+var transactions = [];
+
+//defining common functions
+function addBill(bill, q){
     var exists = false;
     var position;
     for(x in bills){
@@ -40,10 +42,47 @@ function addBills(bill, q){
     }
     //sort bills descending
     bills.sort(function(a, b){
-        return b.type-a.type
+        return b.type-a.type;
     })
 }
 
-function withdraw(q){
+function transactionHistory(q){
+    //generating timestamp of the transaction
+    var currentDate = new Date();
+    var dateString = currentDate.getDate() + "-" +(currentDate.getMonth() + 1) + "-" + currentDate.getFullYear();
+    transactions.push(new Transaction(q,dateString))
+}
 
+function withdraw(q){
+    var total_available = 0;
+    var requested = q;
+    var served = [];
+    for(x of bills){
+        total_available += parseInt(x.totalAvailable());
+    }
+
+    if(requested <= total_available){
+        for(x of bills){
+            if((requested/x.type).toFixed(0) > 0){
+                var count = (requested/x.type).toFixed(0);
+                for(y=0;y<count;y++){
+                    if(x.availableQuantity > 0){
+                        served.push(x.type);
+                        requested -= x.type;
+                        x.availableQuantity -= 1;
+                    }
+                }
+                if(requested%x.type == 0 && x.availableQuantity > 0)
+                {
+                    break;
+                }
+            }
+        }
+
+    return served;
+
+    }
+    else{
+        return false;
+    }
 }
